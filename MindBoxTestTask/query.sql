824bbf1ce5c10c58d506f04d2adc5d19e29097ef
petrov-vo <petrov-vo@keysystems.ru>
2024-04-24 10:37:49 +0300

    второе задание

---
+++ MindBoxTestTask/query.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Product
+(
+	ID INT NOT NULL PRIMARY KEY,
+	"Name" TEXT
+);
+
+CREATE TABLE Category
+(
+	ID INT NOT NULL PRIMARY KEY,
+	"Name" TEXT
+);
+
+CREATE TABLE ProductCategory (
+	ProductId INT FOREIGN KEY REFERENCES Product(Id),
+	CategoryId INT FOREIGN KEY REFERENCES Category(Id),
+	PRIMARY KEY (ProductId, CategoryId)
+);
+
+use test_database
+
+INSERT INTO Product (ID, "Name")
+VALUES 
+(1, 'IPhone'),
+(2, 'NVidia RTX 4090'),
+(3, 'Noname keyboard');
+
+INSERT INTO Category
+VALUES
+	(1, 'Phone'),
+	(2, 'Smartphone'),
+	(3, 'Videocart'),
+	(4, 'Eat');
+
+INSERT INTO ProductCategory
+VALUES
+	(1, 1),
+	(1, 2),
+	(2, 3);
+
+SELECT p."Name", c.Name 
+FROM Product p
+LEFT JOIN ProductCategory pc on p.ID = pc.ProductId
+LEFT JOIN Category c on c.ID = pc.CategoryId;
